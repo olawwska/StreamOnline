debe0241e46ad7fc4e753b6d5e42e62a64279fbb
olawwska <olwojciech@gmail.com>
2020-01-31 18:50:10 +0100

    date to Object

--- index.js
+++ index.js
@@ -1,9 +1,32 @@
 window.addEventListener("DOMContentLoaded", event => {
-  renderTimer = () => {
-    console.log("uruchamia się render timer");
-    let sectionMain = document.querySelector(".section-main");
+    //   renderTimer = () => {
+    //     console.log("uruchamia się render timer");
+    //     let sectionMain = document.querySelector(".section-main");
 
-    sectionMain.innerHTML += `<div class="section-counter">
+    //     sectionMain.innerHTML += `<div class="section-counter">
+    //             <div class="section-counter__day"><p></p><span>DNI</span></div>
+    //             <div class="section-counter__hour"><p></p><span>GODZ</span></div>
+    //             <div class="section-counter__minute"><p></p><span>MIN</span></div>
+    //             <div class="section-counter__second"><p></p><span>SEC</span></div>
+    //             <button class="section-counter__button">
+    //               <i class="fas fa-times"></i>
+    //             </button>
+    //           </div>`;
+    //   };
+
+    let currentTime = target => {
+        let newDate = {
+            day: 0,
+            hour: 0,
+            minute: 0,
+            second: 0
+        };
+        setInterval(() => {
+
+            console.log("uruchamia się render timer");
+            let sectionMain = document.querySelector(".section-main");
+
+            sectionMain.innerHTML += `<div class="section-counter">
             <div class="section-counter__day"><p></p><span>DNI</span></div>
             <div class="section-counter__hour"><p></p><span>GODZ</span></div>
             <div class="section-counter__minute"><p></p><span>MIN</span></div>
@@ -12,73 +35,78 @@ window.addEventListener("DOMContentLoaded", event => {
               <i class="fas fa-times"></i>
             </button>
           </div>`;
-  };
+            console.log(target + 'target');
+            let dateArray = [];
+            let inputDateMs = moment(target, "YYYY-MM-DD HH:mm:ss");
+            let now = moment();
+            let timeDifferenceMs = moment.duration(inputDateMs.diff(now));
+            let timeDifferenceDays = moment.duration(inputDateMs.diff(now, "days"));
+            let timeDifferenceHours = moment.duration(inputDateMs.diff(now, "hours"));
 
-  let currentTime = () => {
-    console.log("uruchamia się current time");
+            newDate.day = Math.floor(Math.abs(timeDifferenceDays.asDays()));
+            newDate.hour = Math.floor(Math.abs(timeDifferenceHours.asHours()));
+            newDate.minute = Math.abs(timeDifferenceMs.minutes());
+            newDate.second = Math.abs(timeDifferenceMs.seconds());
 
-    let newDate = {
-      day: 0,
-      hour: 0,
-      minute: 0,
-      second: 0
+            dateArray.push(newDate);
+
+            let timerValues = document.querySelectorAll(".section-counter>div>p");
+
+            //   let dates = dateArray.map(date => {
+            //     return Object.values(date);
+            //   });
+            //   dates.map((value, i, dates) => {
+            //     console.log(timerValues[i]);
+            //     timerValues[i].innerHTML = value;
+            //   });
+            //   dateArray.map(date => {
+            //     return Object.values(date).map((value, i) => {
+            //       console.log(timerValues[i]);
+            //       //   console.log(value);
+            //       //   timerValues[i].innerText = value;
+            //     });
+            //   });
+
+            //   dateArray
+            //     .map(date => {
+            //       date = Object.values(date);
+            //     })
+            //     .map((value, i) => {
+            //       timerValues[i].innerHTML = value;
+            //     });
+
+            //   console.log(timerValues);
+            timerValues[0].innerHTML = dateArray[0].day;
+            timerValues[1].innerHTML = dateArray[0].hour;
+            timerValues[2].innerHTML = dateArray[0].minute;
+            timerValues[3].innerHTML = dateArray[0].second;
+        }, 1000);
     };
-    setInterval(() => {
-      let dateArray = [];
-      //pobiera wartość inputa
-      let inputDate = document.querySelector(".section-date__input").value;
 
-      //oblicza róznice dwóch czasów w milisekundy
-      let inputDateMs = moment(inputDate, "YYYY-MM-DD HH:mm:ss");
-      let now = moment(Date.now());
-      let timeDifference = moment(now).diff(
-        moment(inputDateMs, "YYYY-MM-DD HH:mm:ss")
-      );
+    saveDateToLocalStorage = newObject => {
+        var dataFromLocalStorage = [];
+        if (localStorage.getItem("dates") != null) {
+            dataFromLocalStorage = JSON.parse(localStorage.getItem("dates"));
+            dataFromLocalStorage.push(newObject);
+            localStorage.setItem("dates", JSON.stringify(dataFromLocalStorage));
+        } else {
+            dataFromLocalStorage.push(newObject);
+            localStorage.setItem("dates", JSON.stringify(dataFromLocalStorage));
+        }
+    };
 
-      let timeDifferenceDuration = moment.duration(timeDifference);
-
-      //wylicza ile to godzin sekund dni i minut
-      newDate.hour = Math.abs(timeDifferenceDuration.hours());
-      newDate.seconds = Math.abs(timeDifferenceDuration.seconds());
-      newDate.day = Math.abs(timeDifferenceDuration.days());
-      newDate.minute = Math.abs(timeDifferenceDuration.minutes());
-
-      //wypycha obiekt do tablicy
-      dateArray.push(newDate);
-
-      let timerValues = document.querySelectorAll(".section-counter>div>p");
-
-      dateArray.map(date => {
-        return Object.values(date).map((value, i) => {
-          console.log(timerValues[i]);
-          //   console.log(value);
-          timerValues[i].innerText = value;
+    document
+        .querySelector(".section-date__button")
+        .addEventListener("click", () => {
+            var inputDate = document.querySelector(".section-date__input").value;
+            saveDateToLocalStorage(inputDate);
+            //   renderTimer(inputDate);
+            currentTime();
         });
-      });
 
-      //   dateArray
-      //     .map(date => {
-      //       date = Object.values(date);
-      //     })
-      //     .map((value, i) => {
-      //       timerValues[i].innerHTML = value;
-      //     });
-
-      //   console.log(timerValues);
-      // timerValues[0].innerHTML = dateArray[0].seconds;
-    }, 1000);
-  };
-
-  document
-    .querySelector(".section-date__button")
-    .addEventListener("click", () => {
-      renderTimer();
-      currentTime();
-    });
-
-  document
-    .querySelector(".section-counter__button")
-    .addEventListener("click", function() {
-      this.parentElement.remove();
-    });
+    document
+        .querySelector(".section-counter__button")
+        .addEventListener("click", function () {
+            this.parentElement.remove();
+        });
 });
---
+++ index2.js
@@ -0,0 +1,56 @@
+window.addEventListener("DOMContentLoaded", () => {
+    let dataFromLocalStorage = [];
+    document
+        .querySelector(".section-date__button")
+        .addEventListener("click", () => {
+
+            //renderTimer();
+
+            // let sectionMain = document.querySelector(".section-main");
+
+            // sectionMain.innerHTML += `<div class="section-counter">
+            //     <div class="section-counter__day"><p></p><span>DNI</span></div>
+            //     <div class="section-counter__hour"><p></p><span>GODZ</span></div>
+            //     <div class="section-counter__minute"><p></p><span>MIN</span></div>
+            //     <div class="section-counter__second"><p></p><span>SEC</span></div>
+            //     <button class="section-counter__button">
+            //       <i class="fas fa-times"></i>
+            //     </button>
+            //   </div>`;
+
+            // currentTime();
+            let inputDate = document.querySelector('.section-date__input').value;
+            let dateObject = {
+                day: 0,
+                hour: 0,
+                minute: 0,
+                second: 0
+            };
+            setInterval(() => {
+                let timeDifference = new Date(inputDate).getTime() - Date.now();
+                let days = Math.floor(timeDifference / 86400000);
+                let hours = Math.floor((timeDifference - (days * 86400000)) / 3600000);
+                let minutes = Math.floor((timeDifference - (days * 86400000) - (hours * 3600000)) / 60000);
+                let seconds = Math.floor((timeDifference - (days * 86400000) - (hours * 3600000) - (minutes * 60000)) / 1000);
+
+                dateObject.day = days;
+                dateObject.hour = hours;
+                dateObject.minute = minutes;
+                dateObject.second = seconds;
+
+                var dataFromLocalStorage = [];
+                if (localStorage.getItem("dates") != null) {
+                    dataFromLocalStorage = JSON.parse(localStorage.getItem("dates"));
+                    dataFromLocalStorage.shift();
+                    dataFromLocalStorage.push(dateObject);
+                    localStorage.setItem("dates", JSON.stringify(dataFromLocalStorage));
+                } else {
+                    dataFromLocalStorage.shift();
+                    dataFromLocalStorage.push(dateObject);
+                    localStorage.setItem("dates", JSON.stringify(dataFromLocalStorage));
+                }
+                console.log(dataFromLocalStorage);
+
+            }, 1000);
+        });
+});
