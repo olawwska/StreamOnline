fca85d34fd9e0b762b67ffac9eb2e78952ee2cac
olawwska <olwojciech@gmail.com>
2020-02-03 00:28:11 +0100

    wrapped into functions

--- index.js
+++ index.js
@@ -1,98 +1,65 @@
-window.addEventListener("DOMContentLoaded", event => {
-  renderTimer = () => {
-    console.log("uruchamia się render timer");
-    let sectionMain = document.querySelector(".section-main");
+function initLocalStorage() {
+  localStorage.setItem("dates", JSON.stringify([]));
+}
 
-    sectionMain.innerHTML += `<div class="section-counter">
-            <div class="section-counter__day"><p></p><span>DNI</span></div>
-            <div class="section-counter__hour"><p></p><span>GODZ</span></div>
-            <div class="section-counter__minute"><p></p><span>MIN</span></div>
-            <div class="section-counter__second"><p></p><span>SEC</span></div>
-            <button class="section-counter__button">
-              <i class="fas fa-times"></i>
-            </button>
-          </div>`;
-  };
+function saveDateInStorage(inputDate) {
+  let dataFromLocalStorage = JSON.parse(localStorage.getItem("dates"));
+  dataFromLocalStorage.push(inputDate);
+  localStorage.setItem("dates", JSON.stringify(dataFromLocalStorage));
+}
 
-  let currentTime = () => {
-    let newDate = {
-      day: 0,
-      hour: 0,
-      minute: 0,
-      second: 0
-    };
-    setInterval(() => {
-      let dateArray = [];
-      var inputDate = document.querySelector(".section-date__input").value;
-      let inputDateMs = moment(inputDate, "YYYY-MM-DD HH:mm:ss");
-      let now = moment();
-      let timeDifferenceMs = moment.duration(inputDateMs.diff(now));
-      let timeDifferenceDays = moment.duration(inputDateMs.diff(now, "days"));
-      let timeDifferenceHours = moment.duration(inputDateMs.diff(now, "hours"));
+function renderSectionCounter(days, hours, minutes, seconds) {
+  return `<div class="section-counter">
+                <div class="section-counter__element"><p>${days}</p><span>DNI</span></div>
+                <div class="section-counter__element"><p>${hours}</p><span>GODZ</span></div>
+                <div class="section-counter__element"><p>${minutes}</p><span>MIN</span></div>
+                <div class="section-counter__element"><p>${seconds}</p><span>SEC</span></div>
+                <button class="section-counter__button">
+                  <i class="fas fa-times"></i>
+                </button>
+              </div>`;
+}
 
-      newDate.day = Math.floor(Math.abs(timeDifferenceDays.asDays()));
-      newDate.hour = Math.floor(Math.abs(timeDifferenceHours.asHours()));
-      newDate.minute = Math.abs(timeDifferenceMs.minutes());
-      newDate.second = Math.abs(timeDifferenceMs.seconds());
+function clearSection(section) {
+  section.innerHTML = '<div></div>';
+}
 
-      dateArray.push(newDate);
+initLocalStorage();
 
-      let timerValues = document.querySelectorAll(".section-counter>div>p");
-
-      //   let dates = dateArray.map(date => {
-      //     return Object.values(date);
-      //   });
-      //   dates.map((value, i, dates) => {
-      //     console.log(timerValues[i]);
-      //     timerValues[i].innerHTML = value;
-      //   });
-      //   dateArray.map(date => {
-      //     return Object.values(date).map((value, i) => {
-      //       console.log(timerValues[i]);
-      //       //   console.log(value);
-      //       //   timerValues[i].innerText = value;
-      //     });
-      //   });
-
-      //   dateArray
-      //     .map(date => {
-      //       date = Object.values(date);
-      //     })
-      //     .map((value, i) => {
-      //       timerValues[i].innerHTML = value;
-      //     });
-
-      //   console.log(timerValues);
-      timerValues[0].innerHTML = dateArray[0].day;
-      timerValues[1].innerHTML = dateArray[0].hour;
-      timerValues[2].innerHTML = dateArray[0].minute;
-      timerValues[3].innerHTML = dateArray[0].second;
-    }, 1000);
-  };
-
-  saveDateToLocalStorage = newObject => {
-    var dataFromLocalStorage = [];
-    if (localStorage.getItem("dates") != null) {
-      dataFromLocalStorage = JSON.parse(localStorage.getItem("dates"));
-      dataFromLocalStorage.push(newObject);
-      localStorage.setItem("dates", JSON.stringify(dataFromLocalStorage));
-    } else {
-      dataFromLocalStorage.push(newObject);
-      localStorage.setItem("dates", JSON.stringify(dataFromLocalStorage));
-    }
-  };
+document.addEventListener("DOMContentLoaded", function () {
+  let dataFromLocalStorage = [];
 
   document
     .querySelector(".section-date__button")
-    .addEventListener("click", () => {
-      saveDateToLocalStorage(inputDate);
-      renderTimer(inputDate);
-      currentTime();
+    .addEventListener("click", function () {
+      let inputDate = this.nextElementSibling.value;
+      saveDateInStorage(inputDate);
     });
 
-  document
-    .querySelector(".section-counter__button")
-    .addEventListener("click", function() {
-      this.parentElement.remove();
-    });
+  setInterval(() => {
+    let sectionMain = document.querySelector(".section-main");
+    clearSection(sectionMain);
+
+    dataFromLocalStorage = JSON.parse(localStorage.getItem("dates"));
+
+    dataFromLocalStorage.forEach(inputDate => {
+
+      let timeDifference = new Date(inputDate).getTime() - Date.now();
+      let days = Math.floor(timeDifference / 86400000);
+      let hours = Math.floor((timeDifference - days * 86400000) / 3600000);
+      let minutes = Math.floor(
+        (timeDifference - days * 86400000 - hours * 3600000) / 60000
+      );
+      let seconds = Math.floor(
+        (timeDifference -
+          days * 86400000 -
+          hours * 3600000 -
+          minutes * 60000) /
+        1000
+      );
+
+      sectionMain.innerHTML += renderSectionCounter(days, hours, minutes, seconds);
+
+    })
+  }, 1000);
 });
